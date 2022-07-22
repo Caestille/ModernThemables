7a11ec204811de264e48b44a8699872360032c82
Joseph Ward <jward@mercedesamgf1.com>
2022-07-22 14:59:07 +0100

    Add ability for circular progress bar to be 'indeterminate' and spin a la google loading ring

--- Controls/ThemableCircularProgressBar.xaml.cs
+++ Controls/ThemableCircularProgressBar.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 
@@ -6,11 +7,21 @@ namespace Win10Themables.Controls
 {
 	public partial class CircularProgressBar
 	{
+		private double indeterminateAngle1;
+		private double indeterminateAngle2;
+		private bool isIndeterminate;
+		private const int lowSpeed = 5;
+		private const int highSpeed = 10;
+		private int indeterminateAngle1Speed = 5;
+		private int indeterminateAngle2Speed = 10;
+		private int sameSpeedCount;
+		private bool setHigh;
+
 		public CircularProgressBar()
 		{
 			InitializeComponent();
-			Angle = (Percentage * 360) / 100;
-			RenderArc();
+			var angle = (Percentage * 360) / 100;
+			RenderArc(0, angle);
 		}
 
 		public int Radius
@@ -37,57 +48,119 @@ namespace Win10Themables.Controls
 			set => SetValue(PercentageProperty, value);
 		}
 
-		public double Angle
+		public bool IsIndeterminate
 		{
-			get => (double)GetValue(AngleProperty);
-			set => SetValue(AngleProperty, value);
+			get => (bool)GetValue(IsIndeterminateProperty);
+			set => SetValue(IsIndeterminateProperty, value);
 		}
 
-		// Using a DependencyProperty as the backing store for Percentage.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty PercentageProperty =
 			DependencyProperty.Register("Percentage", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(65d, OnPercentageChanged));
 
-		// Using a DependencyProperty as the backing store for StrokeThickness.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty StrokeThicknessProperty =
 			DependencyProperty.Register("StrokeThickness", typeof(int), typeof(CircularProgressBar), new PropertyMetadata(5, OnThicknessChanged));
 
-		// Using a DependencyProperty as the backing store for SegmentColor.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty SegmentColorProperty =
 			DependencyProperty.Register("SegmentColor", typeof(Brush), typeof(CircularProgressBar), new PropertyMetadata(new SolidColorBrush(Colors.Red), OnColorChanged));
 
-		// Using a DependencyProperty as the backing store for Radius.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty RadiusProperty =
 			DependencyProperty.Register("Radius", typeof(int), typeof(CircularProgressBar), new PropertyMetadata(25, OnPropertyChanged));
 
-		// Using a DependencyProperty as the backing store for Angle.  This enables animation, styling, binding, etc...
-		public static readonly DependencyProperty AngleProperty =
-			DependencyProperty.Register("Angle", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(120d, OnPropertyChanged));
+		public static readonly DependencyProperty IsIndeterminateProperty =
+			DependencyProperty.Register("IsIndeterminate", typeof(bool), typeof(CircularProgressBar), new PropertyMetadata(false, OnSetIndeterminate));
 
 		private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
-			CircularProgressBar? circle = sender as CircularProgressBar;
-			circle?.set_Color((SolidColorBrush)args.NewValue);
+			CircularProgressBar? this_ = sender as CircularProgressBar;
+			this_?.set_Color((SolidColorBrush)args.NewValue);
 		}
 
 		private static void OnThicknessChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
-			CircularProgressBar? circle = sender as CircularProgressBar;
-			circle?.set_tick((int)args.NewValue);
+			CircularProgressBar? this_ = sender as CircularProgressBar;
+			this_?.set_tick((int)args.NewValue);
 		}
 
 		private static void OnPercentageChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
-			CircularProgressBar? circle = sender as CircularProgressBar;
-			if (circle?.Percentage > 100) 
-				circle.Percentage = 100;
-			if (circle != null) 
-				circle.Angle = (circle.Percentage * 360) / 100;
+			var this_ = sender as CircularProgressBar;
+
+			if (this_ == null || this_.IsIndeterminate)
+				return;
+
+			if (this_?.Percentage > 100)
+				this_.Percentage = 100;
+
+			if (this_ != null)
+			{
+				var angle = (this_.Percentage * 360) / 100;
+				this_?.RenderArc(0, angle);
+			}
 		}
 
 		private static void OnPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
-			CircularProgressBar? circle = sender as CircularProgressBar;
-			circle?.RenderArc();
+			CircularProgressBar? this_ = sender as CircularProgressBar;
+
+			if (this_.IsIndeterminate)
+				this_?.RenderArc(this_.indeterminateAngle1, this_.indeterminateAngle2);
+			else
+				this_?.RenderArc(0, (this_.Percentage * 360) / 100);
+		}
+
+		private static void OnSetIndeterminate(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+		{
+			var this_ = sender as CircularProgressBar;
+			this_.isIndeterminate = this_.IsIndeterminate;
+			if (this_.IsIndeterminate)
+			{
+				this_.indeterminateAngle1 = 0;
+				this_.indeterminateAngle2 = 45;
+
+				Thread thread = new Thread(new ThreadStart(() =>
+				{
+					while (this_.isIndeterminate)
+					{
+						var nextAngle2 = (this_.indeterminateAngle2 + this_.indeterminateAngle2Speed) % 360;
+						var nextAngle1 = (this_.indeterminateAngle1 + this_.indeterminateAngle1Speed) % 360;
+						var diff = 360 - Math.Abs((nextAngle2 < nextAngle1 ? nextAngle1 - (nextAngle2 + 360) : nextAngle1 - nextAngle2));
+						var allowedDiff = 50;
+						if (diff < allowedDiff)
+						{
+							this_.indeterminateAngle2Speed = lowSpeed;
+							this_.setHigh = false;
+						}
+
+						if (diff > (360 - allowedDiff))
+						{
+							this_.indeterminateAngle1Speed = lowSpeed;
+							this_.setHigh = true;
+						}
+
+						if (this_.indeterminateAngle1Speed == this_.indeterminateAngle2Speed)
+						{
+							this_.sameSpeedCount++;
+						}
+
+						if (this_.sameSpeedCount == 30)
+						{
+							this_.sameSpeedCount = 0;
+							if (this_.setHigh)
+								this_.indeterminateAngle2Speed = highSpeed;
+							else
+								this_.indeterminateAngle1Speed = highSpeed;
+						}
+
+						this_.indeterminateAngle1 += this_.indeterminateAngle1Speed;
+						this_.indeterminateAngle2 += this_.indeterminateAngle2Speed;
+						this_.indeterminateAngle1 %= 360;
+						this_.indeterminateAngle2 %= 360;
+						Application.Current.Dispatcher.Invoke(() => this_?.RenderArc(this_.indeterminateAngle1, this_.indeterminateAngle2));
+						Thread.Sleep(1000 / 60);
+					}
+				}));
+				thread.Start();
+			}
 		}
 
 		public void set_tick(int n)
@@ -100,18 +173,22 @@ namespace Win10Themables.Controls
 			PathRoot.Stroke = n;
 		}
 
-		public void RenderArc()
+		public void RenderArc(double angle1, double angle2)
 		{
-			Point startPoint = new Point(Radius, 0);
-			Point endPoint = ComputeCartesianCoordinate(Angle, Radius);
-			endPoint.X += Radius;
-			endPoint.Y += Radius;
+			if (angle1 < 0)
+				angle1 += 360;
+			if (angle2 < 0)
+				angle2 += 360;
+
+			Point startPoint = ComputeCartesianCoordinate(angle1, Radius);
+			Point endPoint = ComputeCartesianCoordinate(angle2, Radius);
 
 			PathRoot.Width = Radius * 2 + StrokeThickness;
 			PathRoot.Height = Radius * 2 + StrokeThickness;
 			PathRoot.Margin = new Thickness(StrokeThickness, StrokeThickness, 0, 0);
 
-			bool largeArc = Angle > 180.0;
+			var diff = angle2 < angle1 ? (angle2 + 360) - angle1 : angle2 - angle1;
+			bool largeArc = diff > 180.0;
 
 			Size outerArcSize = new Size(Radius, Radius);
 
@@ -120,6 +197,7 @@ namespace Win10Themables.Controls
 			if (Math.Abs(startPoint.X - Math.Round(endPoint.X)) < 0.00001 && Math.Abs(startPoint.Y - Math.Round(endPoint.Y)) < 0.00001)
 				endPoint.X -= 0.01;
 
+			arcSegment.RotationAngle = angle1;
 			arcSegment.Point = endPoint;
 			arcSegment.Size = outerArcSize;
 			arcSegment.IsLargeArc = largeArc;
@@ -127,13 +205,13 @@ namespace Win10Themables.Controls
 
 		private Point ComputeCartesianCoordinate(double angle, double radius)
 		{
-			// convert to radians
+			// convert to radians and translate
 			double angleRad = (Math.PI / 180.0) * (angle - 90);
 
 			double x = radius * Math.Cos(angleRad);
 			double y = radius * Math.Sin(angleRad);
 
-			return new Point(x, y);
+			return new Point(x + radius, y + radius);
 		}
 	}
 }
