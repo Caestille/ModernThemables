16942a8082f7b0f8a088db123dc1ce70e9271b98
Joseph <jward2100@gmail.com>
2022-08-17 11:48:48 +0100

    Replace extended button user control with themablebutton custom control and style

---
+++ Controls/ThemableButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Win10Themables.Controls
+{
+	public class ThemableButton : Button
+	{
+		static ThemableButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ThemableButton), new FrameworkPropertyMetadata(typeof(ThemableButton)));
+		}
+
+		readonly static SolidColorBrush DefaultMouseOverProperty = new BrushConverter().ConvertFromString("#FFBEE6FD") as SolidColorBrush;
+
+		public SolidColorBrush MouseOverColour
+		{
+			get { return (SolidColorBrush)GetValue(MouseOverColourProperty); }
+			set { SetValue(MouseOverColourProperty, value); }
+		}
+		public static readonly DependencyProperty MouseOverColourProperty = DependencyProperty.Register(
+		  "MouseOverColour", typeof(SolidColorBrush), typeof(ThemableButton), new PropertyMetadata(DefaultMouseOverProperty));
+
+		public SolidColorBrush MouseDownColour
+		{
+			get { return (SolidColorBrush)GetValue(MouseDownColourProperty); }
+			set { SetValue(MouseDownColourProperty, value); }
+		}
+		public static readonly DependencyProperty MouseDownColourProperty = DependencyProperty.Register(
+		  "MouseDownColour", typeof(SolidColorBrush), typeof(ThemableButton), new PropertyMetadata(DefaultMouseOverProperty));
+
+		public SolidColorBrush DisabledBackgroundColour
+		{
+			get { return (SolidColorBrush)GetValue(DisabledBackgroundColourProperty); }
+			set { SetValue(DisabledBackgroundColourProperty, value); }
+		}
+		public static readonly DependencyProperty DisabledBackgroundColourProperty = DependencyProperty.Register(
+		  "DisabledBackgroundColour", typeof(SolidColorBrush), typeof(ThemableButton), new PropertyMetadata(DefaultMouseOverProperty));
+
+		public SolidColorBrush DisabledForegroundColour
+		{
+			get { return (SolidColorBrush)GetValue(DisabledForegroundColourProperty); }
+			set { SetValue(DisabledForegroundColourProperty, value); }
+		}
+		public static readonly DependencyProperty DisabledForegroundColourProperty = DependencyProperty.Register(
+		  "DisabledForegroundColour", typeof(SolidColorBrush), typeof(ThemableButton), new PropertyMetadata(DefaultMouseOverProperty));
+
+		public CornerRadius CornerRadius
+		{
+			get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+			set { SetValue(CornerRadiusProperty, value); }
+		}
+		public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+		  "CornerRadius", typeof(CornerRadius), typeof(ThemableButton), new PropertyMetadata(new CornerRadius(0)));
+	}
+}
