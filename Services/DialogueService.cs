7a8756673f584ffccea5e5ad888af11ef3466440
Joseph <jward2100@gmail.com>
2022-08-18 02:12:29 +0100

    Can set dialogue size. Open file dialogue returns its loaded path

--- Services/DialogueService.cs
+++ Services/DialogueService.cs
@@ -1,9 +1,11 @@
 ﻿using CoreUtilities.Interfaces;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shell;
 using Win10Themables.Controls;
@@ -19,7 +21,7 @@ namespace Win10Themables.Services
 			registeredViews[vmType] = viewType;
 		}
 
-		public async Task OpenCustomDialogue(object? dataContext=null)
+		public async Task OpenCustomDialogue(object? dataContext=null, Size? dialogueSize = null)
 		{
 			Window window = new Window();
 			window.AllowsTransparency = false;
@@ -29,14 +31,23 @@ namespace Win10Themables.Services
 			window.SnapsToDevicePixels = true;
 			window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
 			window.Owner = Application.Current.MainWindow;
+			window.HorizontalContentAlignment = HorizontalAlignment.Center;
+			window.VerticalContentAlignment = VerticalAlignment.Center;
 			window.Resources.Add(new DataTemplateKey(dataContext.GetType()), new DataTemplate()
 			{
 				DataType = dataContext.GetType(),
 				VisualTree = new FrameworkElementFactory(registeredViews[dataContext.GetType()]),
 			});
-			window.Width = 100;
-			window.Height = 100;
 			window.Content = new MainWindowControl() { VisibleViewModel = (ObservableObject)dataContext, IsToolWindow = true };
+			if (dialogueSize == null)
+			{
+				window.SizeToContent = SizeToContent.WidthAndHeight;
+			}
+			else
+			{
+				window.Width = dialogueSize.Value.Width;
+				window.Height = dialogueSize.Value.Height;
+			}
 			WindowChrome.SetWindowChrome(window, new WindowChrome() { ResizeBorderThickness = new Thickness(5), CaptionHeight = 24 });
 			window.WindowStyle = WindowStyle.ToolWindow;
 			RenderOptions.SetBitmapScalingMode(window, BitmapScalingMode.HighQuality);
@@ -45,9 +56,16 @@ namespace Win10Themables.Services
 			window.ShowDialog();
 		}
 
-		public void OpenFileDialogue()
+		public string OpenFileDialogue()
 		{
+			var dialogue = new OpenFileDialog();
 
+			if (dialogue.ShowDialog() == true)
+			{
+				return dialogue.FileName;
+			}
+
+			return string.Empty;
 		}
 
 		public void ShowMessageBox(string title, string message, MessageBoxButton button)
