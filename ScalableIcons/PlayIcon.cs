999945773bbc55ddf26b6746da47b06e92847895
Joseph <jward2100@gmail.com>
2022-09-20 23:06:23 +0100

    More icons

---
+++ ScalableIcons/PlayIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ModernThemables.ScalableIcons
+{
+	public class PlayIcon : BaseIcon
+	{
+		static PlayIcon()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(PlayIcon), new FrameworkPropertyMetadata(typeof(PlayIcon)));
+		}
+	}
+}
---
+++ ScalableIcons/TagIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ModernThemables.ScalableIcons
+{
+	public class TagIcon : BaseIcon
+	{
+		static TagIcon()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(TagIcon), new FrameworkPropertyMetadata(typeof(TagIcon)));
+		}
+	}
+}
