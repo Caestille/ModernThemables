8cb94fafdca3ba8443e6dae6a326dafa03f89b73
Joseph Ward <jward@mercedesamgf1.com>
2023-06-07 16:33:34 +0100

    Add save icon

---
+++ ScalableIcons/SaveIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ModernThemables.ScalableIcons
+{
+	public class SaveIcon : BaseIcon
+	{
+		static SaveIcon()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(SaveIcon), new FrameworkPropertyMetadata(typeof(SaveIcon)));
+		}
+	}
+}
